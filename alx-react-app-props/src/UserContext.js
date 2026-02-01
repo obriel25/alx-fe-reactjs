31bae5e849f5f3af6a8153d73714601a440cfb4b
Obriel <obrieljefferson@gmail.com>
2026-02-01 08:13:56 +0300

    update

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
+import { createContext } from "react";
 
-const userContext = React.createContext();
+const UserContext = createContext();
 
-export default userContext;
+export default UserContext;
