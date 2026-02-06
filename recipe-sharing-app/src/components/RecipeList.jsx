838976c8bce35776779ebf2d4c0c60a84a9a3115
Obriel <obrieljefferson@gmail.com>
2026-02-06 23:10:40 +0300

    update

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,18 +1,18 @@
-import {useRecipeStore} from './recipeStore';
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
-  if (recipes.length === 0) {
-    return <p>No recipes yet. Add one </p>;
-  }
-
   return (
     <div>
       {recipes.map((recipe) => (
-        <div key={recipe.id} style={{ marginBottom: '1rem' }}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
+        <div key={recipe.id}>
+          <h3>
+            <Link to={`/recipes/${recipe.id}`}>
+              {recipe.title}
+            </Link>
+          </h3>
         </div>
       ))}
     </div>
